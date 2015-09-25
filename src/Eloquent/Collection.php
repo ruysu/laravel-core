acafef207974941080d6ba64a89c03fb511cfba4
Gerardo Gómez <code@gerardo.im>
2015-09-25 08:03:32 -0600

    fixed collection load in my related

--- src/Eloquent/Collection.php
+++ src/Eloquent/Collection.php
@@ -60,24 +60,34 @@ class Collection extends BaseCollection
     {
         $auth = app('auth');
         $user = $auth->user();
-        $user_id = $user ? $user->getKey() : 0;
+        $user_id = $auth->id();
 
         if (count($this->items) > 0 && $user_id) {
             $model = $this->first();
             $property_name = "in_my_{$relation}";
-            $relation = $model->$relation();
+            $relation = $user->$relation();
             $related = $relation->getRelated();
-            $parent_key = $relation->getPlainForeignKey();
 
-            $query = $related->newQuery()
-                ->where('user_id', '=', $user->getKey())
+            if ($relation instanceof BelongsToMany || $relation instanceof MorphToMany) {
+                $parent_key = last(explode('.', $relation->getOtherKey()));
+                $query = $relation->newPivotStatement();
+
+                if ($relation instanceof MorphToMany) {
+                    $query->where($relation->getPlainMorphType(), $relation->getMorphClass());
+                }
+            } else {
+                $parent_key = $relation->getPlainForeignKey();
+                $query = $related->newQuery();
+
+                if ($relation instanceof MorphOneOrMany) {
+                    $query->where($relation->getPlainMorphType(), $relation->getMorphClass());
+                }
+            }
+
+            $query->where('user_id', '=', $user->getKey())
                 ->whereIn($parent_key, $this->lists($model->getKeyName()))
                 ->selectRaw($parent_key . ', count(*) as relation_count')
                 ->groupBy($parent_key);
-
-            if ($relation instanceof MorphOneOrMany) {
-                $query->where($relation->getPlainMorphType(), $relation->getMorphClass());
-            }
 
             $results = $query->lists('relation_count', $parent_key);
 
